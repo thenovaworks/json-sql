3ff49302cbd75e0fe4f400e5d73f192dfaa21ec7
symplesims <symplesims@gmail.com>
2024-07-12 21:42:26 +0900

    ResultMapUtils - java static 메서드 지원

--- src/main/kotlin/io/github/thenovaworks/json/query/ResultMapUtils.kt
+++ src/main/kotlin/io/github/thenovaworks/json/query/ResultMapUtils.kt
@@ -3,10 +3,12 @@ package io.github.thenovaworks.json.query
 class ResultMapUtils {
     companion object {
 
+        @JvmStatic
         fun toMap(obj: Any?): Map<String, Any> {
             return obj as Map<String, Any>
         }
 
+        @JvmStatic
         fun toList(obj: Any?): List<Map<String, Any>> {
             return obj as List<Map<String, Any>>
         }
